6940c8db03dd117fe3aa6271f22d1d91be15e7a2
James Wylde <jamescwylde@microsoft.com>
2023-03-04 22:56:50 +0000

    Comments on NLP added

--- chatgpt-prompt.py
+++ chatgpt-prompt.py
@@ -2,21 +2,23 @@ import openai
 import sys
 import spacy
 import colorama
-from colorama import Fore, Back, Style
+from colorama import Fore
 
 openai.api_key = "API_KEY"
 
 messages = []
 
+# Using Spacy module, load english language model to assist with natural language processing
 nlp = spacy.load('en_core_web_sm')
 
 if len(sys.argv) > 1:
+    # If there is a command line argument, join all the arguments into a single string
     prompt = " ".join(sys.argv[1:])
-    print(Fore.YELLOW + "\nChatGPT: ")
 else:
     print(Fore.YELLOW + "\nChatGPT: " + "How can I help?\n")
     prompt = input(Fore.BLUE + "You: ")
 
+    # Extract noun phrases from the input prompt and join into a single string
     prompt_doc = nlp(prompt)
     prompt_noun_phrases = [chunk.text for chunk in prompt_doc.noun_chunks]
     prompt = " ".join(prompt_noun_phrases)
@@ -26,6 +28,7 @@ while True:
                 print(Fore.YELLOW + "\nChatGPT: " + "Bye")
                 break
             messages.append({"role": "user", "content": prompt})
+            # Using GPT-3.5-turbo model with a temperature of 0.5 for more focused response
             response = openai.ChatCompletion.create(
                 model="gpt-3.5-turbo",
                 temperature=0.5,
