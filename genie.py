cbaee6ebcce91fcde0adb5df6731c04ae4d4fc47
James Wylde <jameswylde@microsoft.com>
2023-03-29 14:15:16 +0100

    Update genie.py
    Added --model argument to switch between ChatGPT API models as needed - default to gpt-4

--- genie.py
+++ genie.py
@@ -3,8 +3,17 @@ import sys
 import random
 import string
 import colorama
+import argparse
 from extras import greeting, lamp
 from colorama import Fore, Back, Style
+
+def parse_args():
+    parser = argparse.ArgumentParser(description="Genie: Interact with ChatGPT")
+    parser.add_argument("--model", default="gpt-4", choices=["gpt-4", "gpt-3.5-turbo","code-davinci-002","text-davinci-003"], help="Choose the API model")
+    parser.add_argument("question", nargs="*", help="Optional question for non-interactive mode")
+    return parser.parse_args()
+
+args = parse_args()
 
 openai.api_key = "API_KEY"
 
@@ -12,8 +21,8 @@ messages = []
 
 randomgreeting = random.choice(greeting)
 
-if len(sys.argv) > 1:
-    prompt = " ".join(sys.argv[1:]).rstrip(string.punctuation)
+if args.question:
+    prompt = " ".join(args.question).rstrip(string.punctuation)
 else:
     print(Fore.YELLOW + lamp)
     print(Fore.YELLOW + "\nGenie: " + randomgreeting + "\n")
@@ -25,16 +34,15 @@ while True:
 
     messages.append({"role": "user", "content": prompt})
     response = openai.ChatCompletion.create(
-        model="gpt-4",  # Replace with the GPT-4 model name
+        model=args.model,
         messages=messages,
         temperature=0.7)
     reply = response["choices"][0]["message"]["content"]
 
     messages.append({"role": "assistant", "content": reply})
     print(Fore.YELLOW + "\nGenie: " + reply + "\n")
-    
-    if len(sys.argv) > 1:
-            break
-    else:
-            prompt = input(Fore.BLUE + "Master: ")
 
+    if args.question:
+        break
+    else:
+        prompt = input(Fore.BLUE + "Master: ")
