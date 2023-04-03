163d5d8ad825a9b2ead599a6fb2a7bc6c0beb616
James Wylde <jamescwylde@microsoft.com>
2023-04-03 21:00:53 +0100

    Added prompts and prompt menu

--- genie.py
+++ genie.py
@@ -3,19 +3,50 @@ import sys
 import random
 import string
 import colorama
+import shutil
 import argparse
 from extras import greeting, lamp
+from prompts import prompts
 from colorama import Fore, Back, Style
+
+openai.api_key = "API_KEY"
 
 def parse_args():
     parser = argparse.ArgumentParser(description="Genie: Interact with ChatGPT")
-    parser.add_argument("--model", default="gpt-4", choices=["gpt-4", "gpt-3.5-turbo","code-davinci-002","text-davinci-003"], help="Choose the API model")
+    parser.add_argument("--model", default="gpt-3.5-turbo", choices=["gpt-4", "gpt-3.5-turbo","code-davinci-002","text-davinci-003"], help="Choose the API model to use")
     parser.add_argument("question", nargs="*", help="Optional question for non-interactive mode")
     return parser.parse_args()
 
-args = parse_args()
+def display_prompt_menu():
+    term_width = shutil.get_terminal_size((80, 20)).columns
+    num_columns = 3
+    column_width = term_width // num_columns
+    formatted_prompts = []
 
-openai.api_key = "API_KEY"
+    for i, prompt in enumerate(prompts):
+        formatted_prompt = f"{i + 1} - {prompt.split(':')[0]}"
+        padded_prompt = formatted_prompt.center(column_width)
+        formatted_prompts.append(padded_prompt)
+
+    print(Fore.YELLOW + "Choose a prompt, type your question, or 'q' to quit:\n".center(term_width))
+    for i, formatted_prompt in enumerate(formatted_prompts):
+        print(Fore.YELLOW + formatted_prompt, end="")
+        if (i + 1) % num_columns == 0 and i != len(formatted_prompts) - 1:
+            print()
+    print("\n")
+
+def center_multiline_string(s):
+    term_width = shutil.get_terminal_size((80, 20)).columns
+    centered_lines = []
+
+    for line in s.split('\n'):
+        padding_left = (term_width - len(line)) // 2
+        centered_line = " " * padding_left + line
+        centered_lines.append(centered_line)
+
+    return '\n'.join(centered_lines)
+
+args = parse_args()
 
 messages = []
 
@@ -24,9 +55,16 @@ randomgreeting = random.choice(greeting)
 if args.question:
     prompt = " ".join(args.question).rstrip(string.punctuation)
 else:
-    print(Fore.YELLOW + lamp)
-    print(Fore.YELLOW + "\nGenie: " + randomgreeting + "\n")
-    prompt = input(Fore.BLUE + "Master: ")
+    print(Fore.YELLOW + center_multiline_string(lamp))
+    print(Fore.YELLOW + center_multiline_string(randomgreeting) + "\n")
+    
+    display_prompt_menu()
+    user_input = input(Fore.BLUE + "Ask me any question, choose '1-9', or 'q': ".center(shutil.get_terminal_size((80, 20)).columns)).strip()
+    if user_input.isdigit() and 1 <= int(user_input) <= len(prompts):
+        prompt = prompts[int(user_input) - 1]
+    else:
+        prompt = user_input
+
 while True:
     if prompt.lower() in ["quit", "q", "bye"]:
         print(Fore.YELLOW + "\nGenie: " + "Farewell, master. Until you drag me out of bed again...\n")
@@ -46,3 +84,10 @@ while True:
         break
     else:
         prompt = input(Fore.BLUE + "Master: ")
+        if prompt.lower() == "menu":
+            display_prompt_menu()
+            user_input = input(Fore.BLUE + "Prompt (1-9), custom question, or 'q': ".center(shutil.get_terminal_size((80, 20)).columns)).strip()
+            if user_input.isdigit() and 1 <= int(user_input) <= len(prompts):
+                prompt = prompts[int(user_input) - 1]
+            else:
+                prompt = user_input
---
+++ prompts.py
@@ -0,0 +1,21 @@
+prompts = [
+
+    "Developer interview:\nAct as an interviewer for a software developer position. Ask me technical questions to assess my skills, focusing on programming languages, algorithms, and software design principles. Keep your questions concise and clear, and provide feedback on my responses. Ask one question at a time and wait for my response.",
+
+    "Python tutor:\nAct as a Python programming tutor. Help me understand Python concepts, best practices, and troubleshooting techniques. Provide clear explanations and examples when needed. Correct any misunderstandings or misconceptions I may have.",
+
+    "Web development mentor:\nAct as a mentor for web development, specializing in HTML, CSS, JavaScript, and modern web frameworks. Help me improve my skills by offering guidance, answering questions, and providing tips on best practices. Review any code I provide and give constructive feedback.",
+
+    "Machine learning enthusiast:\nAct as a machine learning enthusiast who loves discussing the latest trends, research, and practical applications of AI. Engage in conversations about various ML algorithms, libraries, and tools. Share your insights and opinions on future developments in the field.",
+
+    "Technical support:\nAct as a technical support agent for a software product or service. Help me resolve any issues, troubleshoot problems, and answer questions about the product or service. Provide clear, step-by-step instructions and maintain a polite, professional tone.",
+
+    "Code reviewer:\nAct as a code reviewer for a software project. Examine the code snippets I provide, focusing on code quality, maintainability, and efficiency. Offer feedback, suggestions for improvements, and identify any potential issues or bugs.",
+
+    "Agile coach:\nAct as an Agile coach, guiding me through the Agile development process and Scrum framework. Help me understand the roles, events, and artifacts in Scrum, and provide advice on how to effectively use Agile principles in my software development projects.",
+
+    "Cybersecurity expert:\nAct as a cybersecurity expert, discussing various aspects of information security, including network security, encryption, and data protection. Help me understand the importance of security measures and best practices for securing applications and systems.",
+
+    "Open source advocate:\nAct as an advocate for open source software. Discuss the benefits and drawbacks of using open source software, and engage in conversations about popular open source projects, licensing, and community involvement. Share your experiences and insights on contributing to open source projects.",
+
+]
